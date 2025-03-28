80b9125ff9ebf73c88c36fd31fe7210ee0d26862
immonroe <immanuelmonroe@gmail.com>
2025-03-28 15:35:51 -0400

    Setup second API call and created function to grab info for weather data using 3 parameters so that the 2nd function had access to things in first API call

--- js/main.js
+++ js/main.js
@@ -1,60 +1,56 @@
 const button = document.querySelector('button')
-const url = 'https://api.nasa.gov/planetary/apod?api_key=NXAAGwyfeIROseR1cQLlLqNzaMag8fYRk6eYt89P'
 
-button.addEventListener('click', () => {
-    let search = document.querySelector('input').value
+// Clear on refresh to update weather with current conditions
+document.querySelector('.results').innerHTML = '';
 
-    // fetch(url + `&date=${search}`)
-    fetch('https://data.nasa.gov/resource/gvk9-iz74.json')
-    .then(res => res.json()) // parse response as JSON
+fetch('https://data.nasa.gov/resource/gvk9-iz74.json')
+    .then(res => res.json())
     .then(data => {
-        // console.log(data)
-
-        for (i=0; i < data.length; i++) {
+        // Using higher order function .forEach() vs. standard for loop
+        data.forEach(facility => {
             const listCenter = document.createElement('h3')
+            const listWeather = document.createElement('p')
             const listCity = document.createElement('p')
             const listState = document.createElement('p')
-            const listCountry= document.createElement('p')
-            const listLatLong= document.createElement('p')
+            const listCountry = document.createElement('p')
+            const listLatLong = document.createElement('p')
 
-            listCenter.textContent = data[i].center
-            listCity.textContent = `City: ${data[i].city}`
-            listState.textContent = `State: ${data[i].state}`
-            listCountry.textContent = `Country: ${data[i].country}`
-            listLatLong.textContent = `Latitude/Longitude: ${data[i].location.latitude},${data[i].location.longitude}`
+            listCenter.textContent = facility.center
+            listCity.textContent = `City: ${facility.city}`
+            listState.textContent = `State: ${facility.state}`
+            listCountry.textContent = `Country: ${facility.country}`
+            listLatLong.textContent = `Latitude/Longitude: ${facility.location.latitude}, ${facility.location.longitude}`
+            listWeather.textContent = ''
 
             document.querySelector('.results').appendChild(listCenter)
             document.querySelector('.results').appendChild(listCity)
             document.querySelector('.results').appendChild(listState)
             document.querySelector('.results').appendChild(listCountry)
             document.querySelector('.results').appendChild(listLatLong)
+            document.querySelector('.results').appendChild(listWeather)
 
-            // Call function to get location + weather of facility
-        }
+            // basically saying if facility.location.length > 0, aka if a value is present/true
+            if (facility.location) {
+                // Added parameters to pass into function
+                getWeather(facility.location.latitude, facility.location.longitude, listWeather)
+            } else {
+                listWeather.textContent = 'No weather data available for this location'
+            }
+        })
     })
-    .catch(err => {
-        console.log(`error ${err}`)
-    })
-})
+    .catch(err => console.log(`Error: ${err}`))
 
-// const buttonTwo = document.querySelector('button')
-// // document.querySelector('.results').style.display = 'none'
-// button.addEventListener('click', () => {
-//     let search = document.querySelector('input').value
-//     const url = `https://api.weatherapi.com/v1/current.json?key=4c455336721b4765a08175859252603&q=${search}&days=1&aqi=yes&alerts=no`
+function getWeather(facilityLatitude, facilityLongitude, listWeather) {
+    const url = `https://api.weatherapi.com/v1/current.json?key=4c455336721b4765a08175859252603&q=${facilityLatitude},${facilityLongitude}`
 
-//     fetch(url) //Make sure to include to search for date
-//     .then(loc => loc.json()) // parse response as JSON
-//     .then(data => {
-//         console.log(loc)
-//         // document.querySelector('.results').style.display = 'block'
-//         // document.querySelector('.cityState').innerHTML = `${loc.location.name}, ${loc.location.region}`
-//         // document.querySelector('.country').innerHTML = loc.location.country
-//         // document.querySelector('.describe').innerHTML = loc.current.condition.text
-//         // document.querySelector('.temp').innerHTML = `${loc.current.temp_f}\u00B0F`
-//         // document.querySelector('img').src = `https:${loc.current.condition.icon}`
-//     })
-//     .catch(err => {
-//         console.log(`error ${err}`)
-//     })
-// })
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            listWeather.textContent = `Weather: ${data.current.temp_f}°F, ${data.current.condition.text}`
+            
+        })
+        .catch(() => {
+            listWeather.textContent = 'Weather data unavailable'
+        })
+}
