bfdb1db2563d2dd248fc3cdd913580db459957e6
immonroe <immanuelmonroe@gmail.com>
2025-03-28 14:00:54 -0400

    Setup first API call to get all needed information to show on HTML page

--- js/main.js
+++ js/main.js
@@ -4,17 +4,32 @@ const url = 'https://api.nasa.gov/planetary/apod?api_key=NXAAGwyfeIROseR1cQLlLqN
 button.addEventListener('click', () => {
     let search = document.querySelector('input').value
 
-    fetch(url + `&date=${search}`) //Make sure to include to search for date
+    // fetch(url + `&date=${search}`)
+    fetch('https://data.nasa.gov/resource/gvk9-iz74.json')
     .then(res => res.json()) // parse response as JSON
     .then(data => {
-        console.log(data)
-        document.querySelector('h2').innerText = data.title
-        document.querySelector('h3').innerText = data.explanation
-        if (data.hdurl) {
-            document.querySelector('img').src = data.hdurl
-            document.querySelector("iframe").style.display = "none";
-        } else {
-            document.querySelector('iframe').src = data.url
+        // console.log(data)
+
+        for (i=0; i < data.length; i++) {
+            const listCenter = document.createElement('h3')
+            const listCity = document.createElement('p')
+            const listState = document.createElement('p')
+            const listCountry= document.createElement('p')
+            const listLatLong= document.createElement('p')
+
+            listCenter.textContent = data[i].center
+            listCity.textContent = `City: ${data[i].city}`
+            listState.textContent = `State: ${data[i].state}`
+            listCountry.textContent = `Country: ${data[i].country}`
+            listLatLong.textContent = `Latitude/Longitude: ${data[i].location.latitude},${data[i].location.longitude}`
+
+            document.querySelector('.results').appendChild(listCenter)
+            document.querySelector('.results').appendChild(listCity)
+            document.querySelector('.results').appendChild(listState)
+            document.querySelector('.results').appendChild(listCountry)
+            document.querySelector('.results').appendChild(listLatLong)
+
+            // Call function to get location + weather of facility
         }
     })
     .catch(err => {
@@ -22,22 +37,22 @@ button.addEventListener('click', () => {
     })
 })
 
-// const button = document.querySelector('button')
+// const buttonTwo = document.querySelector('button')
 // // document.querySelector('.results').style.display = 'none'
 // button.addEventListener('click', () => {
 //     let search = document.querySelector('input').value
 //     const url = `https://api.weatherapi.com/v1/current.json?key=4c455336721b4765a08175859252603&q=${search}&days=1&aqi=yes&alerts=no`
 
 //     fetch(url) //Make sure to include to search for date
-//     .then(res => res.json()) // parse response as JSON
+//     .then(loc => loc.json()) // parse response as JSON
 //     .then(data => {
-//         console.log(data)
-//         document.querySelector('.results').style.display = 'block'
-//         document.querySelector('.cityState').innerHTML = `${data.location.name}, ${data.location.region}`
-//         document.querySelector('.country').innerHTML = data.location.country
-//         document.querySelector('.describe').innerHTML = data.current.condition.text
-//         document.querySelector('.temp').innerHTML = `${data.current.temp_f}\u00B0F`
-//         document.querySelector('img').src = `https:${data.current.condition.icon}`
+//         console.log(loc)
+//         // document.querySelector('.results').style.display = 'block'
+//         // document.querySelector('.cityState').innerHTML = `${loc.location.name}, ${loc.location.region}`
+//         // document.querySelector('.country').innerHTML = loc.location.country
+//         // document.querySelector('.describe').innerHTML = loc.current.condition.text
+//         // document.querySelector('.temp').innerHTML = `${loc.current.temp_f}\u00B0F`
+//         // document.querySelector('img').src = `https:${loc.current.condition.icon}`
 //     })
 //     .catch(err => {
 //         console.log(`error ${err}`)
